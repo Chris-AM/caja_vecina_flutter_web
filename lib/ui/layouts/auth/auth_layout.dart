d3c7b53d76a7061a49e759e60a7fc2eae31999f6
Chris-am <c.aranguizm@outlook.com>
2023-02-17 13:46:23 -0300

    :construction: declaring login scaffold. MISSING => build view

--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mockup_caja_vecina/app_theme.dart';
 
 class AuthLayout extends StatelessWidget {
   const AuthLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Auth Layout'),
+    return Scaffold(
+      body: Stack(
+        children: const [
+          //? Desktop
+          _DesktopBody(),
+          //? Mobile
+          //? LinksBar
+        ],
+      ),
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Center(
+      child: Container(
+        height: size.height,
+        width: size.width,
+        color: AppTheme.loginBackground,
+        child: Column(
+          children: [
+            //? Background
+            Container(
+              color: AppTheme.fontColor,
+            ),
+            Center(
+              child: Container(
+                height: 400,
+                width: 300,
+                color: AppTheme.linksColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
