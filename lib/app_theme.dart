6f63884ed1fea417b6d014623f4c2d6fdb748726
Chris-am <c.aranguizm@outlook.com>
2023-02-17 13:46:50 -0300

    :lipstick: declaring global colors

---
+++ lib/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color loginBackground = Color.fromARGB(255, 255, 255, 255);
+  static const Color fontColor = Color.fromARGB(255, 49, 56, 61);
+  static const Color linksColor = Color.fromARGB(255, 110,131,166); 
+}
